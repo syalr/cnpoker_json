a00f8044f04e861fc1ef6d17f1d2b4f155d12709
yond2015 <yond2015@gmail.com>
2015-09-07 14:03:56 +0800

    modify PacketStruct_Game.h

--- Common/PackageStruct_Game.h
+++ Common/PackageStruct_Game.h
@@ -47,312 +47,255 @@ struct MSG_START_GAME_NAK : public MSG_BASE_FORWARD
 
 /*************************************/
 #if 0
-struct MSG_AG_JOINROOM_REQ : public MSG_BASE_FORWARD
+struct MSG_JOINROOM_REQ : public MSG_BASE_FORWARD
 {
 	unsigned int m_uiRoomNumber;
 	
-	MSG_AG_JOINROOM_REQ() 
+	MSG_JOINROOM_REQ() 
 	{
-		memset( this, 0, sizeof(MSG_AG_JOINROOM_REQ) );
+		memset( this, 0, sizeof(MSG_JOINROOM_REQ) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_JoinRoom_REQ;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = JoinRoom_REQ;
 	}
 };
 
-struct MSG_AG_JOINROOM_ANC : public MSG_BASE_FORWARD
+struct MSG_JOINROOM_ANC : public MSG_BASE_FORWARD
 {
 	//PlayerInfo m_playerInfo;
 	//unsigned int m_byRoomNumber;
 	
-	MSG_AG_JOINROOM_ANC() 
+	MSG_JOINROOM_ANC() 
 	{
-		memset( this, 0, sizeof(MSG_AG_JOINROOM_ANC) );
+		memset( this, 0, sizeof(MSG_JOINROOM_ANC) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_JoinRoom_ANC;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = JoinRoom_ANC;
 	}
 };
 
-struct MSG_AG_JOINTABLE_REQ : public MSG_BASE_FORWARD
+struct MSG_JOINTABLE_REQ : public MSG_BASE_FORWARD
 {
 	unsigned int m_uiTableNumber;
 	//unsigned int m_uiSeat;
 	
-	MSG_AG_JOINTABLE_REQ() 
+	MSG_JOINTABLE_REQ() 
 	{
-		memset( this, 0, sizeof(MSG_AG_JOINTABLE_REQ) );
+		memset( this, 0, sizeof(MSG_JOINTABLE_REQ) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_JoinTable_REQ;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = JoinTable_REQ;
 	}
 };
 
-struct MSG_AG_JOINTABLE_ANC : public MSG_BASE_FORWARD
+struct MSG_JOINTABLE_ANC : public MSG_BASE_FORWARD
 {
 	unsigned int m_uiTableNumber;
 	
-	MSG_AG_JOINTABLE_ANC() 
+	MSG_JOINTABLE_ANC() 
 	{
-		memset( this, 0, sizeof(MSG_AG_JOINTABLE_ANC) );
+		memset( this, 0, sizeof(MSG_JOINTABLE_ANC) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_JoinTable_ANC;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = JoinTable_ANC;
 	}
 };
 
 // 初始化牌
-struct MSG_AG_INITCARDS_BRD : public MSG_BASE_FORWARD
+struct MSG_INITCARDS_BRD : public MSG_BASE_FORWARD
 {
 	BYTE m_byCards[20];
 	
-	MSG_AG_INITCARDS_BRD() 
+	MSG_INITCARDS_BRD() 
 	{
-		memset( this, 0, sizeof(MSG_AG_INITCARDS_BRD) );
+		memset( this, 0, sizeof(MSG_INITCARDS_BRD) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_InitCards_BRD;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = InitCards_BRD;
 	}
 };
 
-struct MSG_AG_INITCARDS_ANC : public MSG_BASE_FORWARD
+struct MSG_INITCARDS_ANC : public MSG_BASE_FORWARD
 {
-	MSG_AG_INITCARDS_ANC() 
+	MSG_INITCARDS_ANC() 
 	{
-		memset( this, 0, sizeof(MSG_AG_INITCARDS_ANC) );
+		memset( this, 0, sizeof(MSG_INITCARDS_ANC) );
 		
-		m_byCategory = AG_Connect;
+		m_byCategory = Games_Protocol;
 		//m_byProtocol = AG_InitCards_ANC;
-		
-		m_dwParameter = 0; // dwUserID
-		m_byParameter = 0;
 	}
 };
 
 // 叫地主 请求
-struct MSG_AG_CALLLANDLOARD_REQ : public MSG_BASE_FORWARD
+struct MSG_CALLLANDLOARD_REQ : public MSG_BASE_FORWARD
 {
 	BYTE m_byCall; // 0 不叫， 1 叫地主
 	
-	MSG_AG_CALLLANDLOARD_REQ()
+	MSG_CALLLANDLOARD_REQ()
 	{
-		memset( this, 0, sizeof(MSG_AG_CALLLANDLOARD_REQ) );
+		memset( this, 0, sizeof(MSG_CALLLANDLOARD_REQ) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_CallLandlord_REQ;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = CallLandlord_REQ;
 	}
 };
 
 // 叫地主 应答
-struct MSG_AG_CALLLANDLOARD_ANC : public MSG_BASE_FORWARD
+struct MSG_CALLLANDLOARD_ANC : public MSG_BASE_FORWARD
 {	
-	MSG_AG_CALLLANDLOARD_ANC()
+	MSG_CALLLANDLOARD_ANC()
 	{
-		memset( this, 0, sizeof(MSG_AG_CALLLANDLOARD_ANC) );
+		memset( this, 0, sizeof(MSG_CALLLANDLOARD_ANC) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_CallLandlord_ANC;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = CallLandlord_ANC;
 	}
 };
 
 // 抢地主 请求
-struct MSG_AG_GRABLANDLOARD_REQ : public MSG_BASE_FORWARD
+struct MSG_GRABLANDLOARD_REQ : public MSG_BASE_FORWARD
 {
-	MSG_AG_GRABLANDLOARD_REQ() 
+	MSG_GRABLANDLOARD_REQ() 
 	{
-		memset( this, 0, sizeof(MSG_AG_GRABLANDLOARD_REQ) );
+		memset( this, 0, sizeof(MSG_GRABLANDLOARD_REQ) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_GrabLandlord_REQ;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = GrabLandlord_REQ;
 	}
 };
 
 // 抢地主 应答
-struct MSG_AG_GRABLANDLOARD_ANC : public MSG_BASE_FORWARD
+struct MSG_GRABLANDLOARD_ANC : public MSG_BASE_FORWARD
 {
 	//unsigned int m_uiSeat; // 成功抢到地主的玩家Key 还是说 Seat ?
 	
-	MSG_AG_GRABLANDLOARD_ANC() 
+	MSG_GRABLANDLOARD_ANC() 
 	{
-		memset( this, 0, sizeof(MSG_AG_GRABLANDLOARD_ANC) );
+		memset( this, 0, sizeof(MSG_GRABLANDLOARD_ANC) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_GrabLandlord_ANC;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = GrabLandlord_ANC;
 	}
 };
 
 // 抢地主 广播 (最终地主确定)
-struct MSG_AG_GRABLANDLOARD_BRD : public MSG_BASE_FORWARD
+struct MSG_GRABLANDLOARD_BRD : public MSG_BASE_FORWARD
 {	
-	MSG_AG_GRABLANDLOARD_BRD() 
+	MSG_GRABLANDLOARD_BRD() 
 	{
-		memset( this, 0, sizeof(MSG_AG_GRABLANDLOARD_BRD) );
+		memset( this, 0, sizeof(MSG_GRABLANDLOARD_BRD) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_GrabLandlord_BRD;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = GrabLandlord_BRD;
 	}
 };
 
 // 明牌
-struct MSG_AG_SHOWCARDS_REQ : public MSG_BASE_FORWARD
+struct MSG_SHOWCARDS_REQ : public MSG_BASE_FORWARD
 {
-	MSG_AG_SHOWCARDS_REQ() 
+	MSG_SHOWCARDS_REQ() 
 	{
-		memset( this, 0, sizeof(MSG_AG_SHOWCARDS_REQ) );
+		memset( this, 0, sizeof(MSG_SHOWCARDS_REQ) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_ShowCards_REQ;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = ShowCards_REQ;
 	}
 };
 
-struct MSG_AG_SHOWCARDS_ANC : public MSG_BASE_FORWARD
+struct MSG_SHOWCARDS_ANC : public MSG_BASE_FORWARD
 {
 	DWORD m_dwUserID;
 	BYTE  m_byCards[CNPOKER_CARD_LEN_2];
 	
-	MSG_AG_SHOWCARDS_ANC() 
+	MSG_SHOWCARDS_ANC() 
 	{
-		memset( this, 0, sizeof(MSG_AG_SHOWCARDS_ANC) );
+		memset( this, 0, sizeof(MSG_SHOWCARDS_ANC) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_ShowCards_ANC;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = ShowCards_ANC;
 	}
 };
 
 // 出牌
-struct MSG_AG_DISCARDS_REQ : public MSG_BASE_FORWARD
+struct MSG_DISCARDS_REQ : public MSG_BASE_FORWARD
 {
 	unsigned int m_uiSize;
 	BYTE m_byDiscards[CNPOKER_CARD_LEN_2]; // 最多20张
 	
-	MSG_AG_DISCARDS_REQ() 
+	MSG_DISCARDS_REQ() 
 	{
-		memset( this, 0, sizeof(MSG_AG_DISCARDS_REQ) );
+		memset( this, 0, sizeof(MSG_DISCARDS_REQ) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_Discards_REQ;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = Discards_REQ;
 	}
 };
 
-struct MSG_AG_DISCARDS_ANC : public MSG_BASE_FORWARD
+struct MSG_DISCARDS_ANC : public MSG_BASE_FORWARD
 {
 	DWORD m_dwUserID;
 	DWORD m_uiSize;
 	BYTE m_byDiscards[CNPOKER_CARD_LEN_2]; // 最多20张
 	
-	MSG_AG_DISCARDS_ANC() 
+	MSG_DISCARDS_ANC() 
 	{
-		memset( this, 0, sizeof(MSG_AG_DISCARDS_ANC) );
+		memset( this, 0, sizeof(MSG_DISCARDS_ANC) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_Discards_ANC;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = Discards_ANC;
 	}
 };
 
 // 出牌 无效 
-struct MSG_AG_DISCARDS_INVALID : public MSG_BASE_FORWARD
+struct MSG_DISCARDS_INVALID : public MSG_BASE_FORWARD
 {
 	DWORD m_dwErrorCode;
 	
-	MSG_AG_DISCARDS_INVALID() 
+	MSG_DISCARDS_INVALID() 
 	{
-		memset( this, 0, sizeof(MSG_AG_DISCARDS_INVALID) );
+		memset( this, 0, sizeof(MSG_DISCARDS_INVALID) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_Discards_INVALID;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = Discards_INVALID;
 	}
 };
 
 // 要不起 请求
-struct MSG_AG_PASS_REQ : public MSG_BASE_FORWARD
+struct MSG_PASS_REQ : public MSG_BASE_FORWARD
 {	
-	MSG_AG_PASS_REQ() 
+	MSG_PASS_REQ() 
 	{
-		memset( this, 0, sizeof(MSG_AG_PASS_REQ) );
+		memset( this, 0, sizeof(MSG_PASS_REQ) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_Pass_REQ;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = Pass_REQ;
 	}
 };
 
 // 要不起 应答
-struct MSG_AG_PASS_ANC : public MSG_BASE_FORWARD
+struct MSG_PASS_ANC : public MSG_BASE_FORWARD
 {
 	//DWORD m_dwUserKey;
 	//DWORD m_uiSize;
 	
-	MSG_AG_PASS_ANC() 
+	MSG_PASS_ANC() 
 	{
-		memset( this, 0, sizeof(MSG_AG_PASS_ANC) );
+		memset( this, 0, sizeof(MSG_PASS_ANC) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_Pass_ANC;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = Pass_ANC;
 	}
 };
 
 // 取得胜利
-struct MSG_AG_WINPLAYER_BRD : public MSG_BASE_FORWARD
+struct MSG_WINPLAYER_BRD : public MSG_BASE_FORWARD
 {	
-	MSG_AG_WINPLAYER_BRD() 
+	MSG_WINPLAYER_BRD() 
 	{
-		memset( this, 0, sizeof(MSG_AG_WINPLAYER_BRD) );
+		memset( this, 0, sizeof(MSG_WINPLAYER_BRD) );
 		
-		m_byCategory = AG_Connect;
-		m_byProtocol = AG_Discards_REQ;
-		
-		m_dwParameter = 0;
-		m_byParameter = 0;
+		m_byCategory = Games_Protocol;
+		m_byProtocol = WinPlayer_BRD;
 	}
 };
 #endif
